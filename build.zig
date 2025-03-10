b7feae206828af30fc21a20ba5ba4f4ad2104016
qtessier <qtessier@keysom.io>
2025-03-10 09:56:42 -0500

    updating build.zig.zon for zig 0.14.0 and name change

--- build.zig
+++ build.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    _ = b.addModule("TureenGraphics", .{
+    _ = b.addModule("Inlucere", .{
         .root_source_file = b.path("src/root.zig"),
     });
 }
