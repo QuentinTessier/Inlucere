da88c7e19a36c275e7ba055e17ae3fd211ec0a98
Quentin Tessier <tessier.quentin.pro@gmail.com>
2025-09-02 04:06:56 -0500

    adding a executable to test binding

--- src/root.zig
+++ src/root.zig
@@ -1,3 +1,15 @@
 const std = @import("std");
 const builtin = @import("builtin");
 const vk = @import("vk.zig");
+
+pub const GPUContext = @This();
+
+allocator: std.mem.Allocator,
+base_wrapper: vk.BaseWrapper,
+instance: vk.Instance,
+
+pub fn init(allocator: std.mem.Allocator, name: [*:0]const u8, loader: *const fn (vk.Instance, [*]const u8) ?*anyopaque) !GPUContext {
+    var self: GPUContext = undefined;
+    self.allocator = allocator;
+    self.base_wrapper = vk.BaseWrapper.load(loader);
+}
